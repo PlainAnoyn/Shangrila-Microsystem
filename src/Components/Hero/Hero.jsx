247fabff0e1fdbc0c7afc8a15aa445d4dad21a0c
Xebec <Xebec@NITRO>
2024-07-20 12:00:00 +0545

    Commit fo

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import './Hero.css'
-import arrow_icon from '../../assets/arrowicon.png'
-const Hero = () => {
+import React from 'react';
+import './Hero.css';
+import arrow_icon from '../../assets/arrowicon.png';
+
+function Hero() {
   return (
-    <div className='hero containerr' id='hero'>
-      <div className='hero-text'>
-<h1>Working towards the excellence in ensuring the Digital Governance in Nepal.</h1>
-      <button className='btnn rgt'>Join US 
-        <img src={arrow_icon} alt="" />
-      </button>
+    <div className="hero containerr" id="hero">
+      <div className="hero-text">
+        <h1>
+          Working towards the excellence in ensuring the Digital Governance in Nepal.
+        </h1>
+        <button className="btnn rgt">
+          Join US
+          <img src={arrow_icon} alt="Arrow Icon" />
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
